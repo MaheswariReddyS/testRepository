6c2444cfb556d88a13df2013db35e582d63ff75f
sivakumar reddy <madhuinfo39@gmail.com>
2018-06-29 10:09:36 +0530

    adding latest file

--- src/main/java/testScripts/GitTest3.java
+++ src/main/java/testScripts/GitTest3.java
@@ -1,5 +1,16 @@
 package testScripts;
 
 public class GitTest3 {
+	
+	public void test1() {
+		int a =10;
+		int b = 30;
+		int c = a+b;
+		System.out.println(c);
+		for(int i=0;i<9;i++) {
+			c=1;
+			
+		}
+	}
 
 }
